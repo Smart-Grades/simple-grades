19f9b01bd182a3c04fa80c13647622ce4c7e08ff
Reza Jaber <frontendrezajaber@gmail.com>
2023-04-16 00:26:44 +0200

    F: Finished Landing-Page, Background-Pattern missing

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    fontFamily: {
+      'main': ['Montserrat', 'sans-serif'],
+      'fancy': ['Source Serif Pro', 'serif']
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+      black: 900
+    },
+    fontSize: {
+      '2xs': '.625rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '4.5rem',
+      '8xl': '5.5rem',
+      '9xl': '7rem',
+      'xxxl': '8.5rem'
+    },
+    extend: {
+      colors: {
+        dark: '#1b1b1b',
+        fom: '#00352e'
+      }
+    },
+  },
+  plugins: [],
+}
