dbb57189d0cf2f735cb8a73e45dc0e8a365e5ab9
Reza Jaber <frontendrezajaber@gmail.com>
2023-04-16 00:26:44 +0200

    F: Finished Landing-Page, Background-Pattern missing

---
+++ components/Steps.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <!-- Steps-Steps -->
+        <div class="mb-16 px-6 space-y-8 || sm:mb-28 sm:px-12 sm:space-y-14 || md:px-20 || xl:px-24 xl:space-y-0 xl:flex xl:justify-between xl:items-center || 2xl:px-44">
+            <div class="flex flex-col items-center || md:flex-row || xl:flex-col xl:w-[425px]">
+                <div class="mb-2 | w-12 h-12 bg-fom rounded-full shadow-2xl | flex justify-center items-center || w-20 h-20">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white || sm:w-10 sm:h-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                    </svg>
+                </div>
+
+                <div class="flex flex-col items-center || md:ml-5 md:items-start || xl:items-center xl:ml-0">
+                    <h2 class="font-main font-extrabold text-xl text-white || sm:text-2xl">Step 1</h2>
+                    <p class="font-main text-white || sm:text-lg">Create an Account or Log In</p>
+                </div>
+            </div>
+
+
+
+            <div class="flex flex-col items-center || md:flex-row || xl:flex-col xl:w-[425px]">
+                <div class="mb-2 | w-12 h-12 bg-fom rounded-full shadow-2xl | flex justify-center items-center || w-20 h-20">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white || sm:w-10 sm:h-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+                    </svg>
+                </div>
+
+                <div class="flex flex-col items-center || md:ml-5 md:items-start || xl:items-center xl:ml-0">
+                    <h2 class="font-main font-extrabold text-xl text-white || sm:text-2xl">Step 2</h2>
+                    <p class="font-main text-white || sm:text-lg">Choose your School and Subject</p>
+                </div>
+            </div>
+
+
+
+            <div class="flex flex-col items-center || md:flex-row || xl:flex-col xl:w-[425px]">
+                <div class="mb-2 | w-12 h-12 bg-fom rounded-full shadow-2xl | flex justify-center items-center || w-20 h-20">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white || sm:w-10 sm:h-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z" />
+                    </svg>
+
+                </div>
+
+                <div class="flex flex-col items-center || md:ml-5 md:items-start || xl:items-center xl:ml-0">
+                    <h2 class="font-main font-extrabold text-xl text-white || sm:text-2xl">Step 3</h2>
+                    <p class="font-main text-white || sm:text-lg">Fill in your Grades and get your Report</p>
+                </div>
+            </div>
+        </div> 
+        
+        <!-- MacBook-Illustration -->
+        <div class="mb-4 flex justify-center || 2xl:mb-20">
+            <img class="w-80 || sm: w-[35rem] || lg:w-[60rem] || xl:w-[65rem] || 2xl:w-[75rem]" src="assets/img/macbookpro.png" alt="illustartion-macbook">
+        </div>
+    </div>
+ </template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="relative">
+        <div class="flex flex-col h-full bg-dark">
+            <Header class="mb-10 || md:mb-20"/>
+            <Hero class="mb-10 || lg:mb-14 || 2xl:mb-20"/>
+            <Steps class="z-20"/>
+        </div>
+        
+        
+        <img class="z-0 absolute bottom-0" src="assets/img/wave.svg" alt="wave-illustration">
+        
+    </div>
+</template>
+
