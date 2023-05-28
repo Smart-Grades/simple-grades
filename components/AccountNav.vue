bdbdcec109e121ffd8d3988f6e38ac1c932da230
rezajaber <frontendrezajaber@gmail.com>
2023-05-28 07:56:56 +0200

    feat: Added Account Page-Design

---
+++ components/AccountNav.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <!-- HEADER -->
+        <div class="">
+            <NuxtLink to="/"><p class="font-main text-xl text-white font-bold cursor-pointer">SmartGrades</p></NuxtLink>
+        </div>
+    </div>
+</template>
