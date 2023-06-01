e62ecedd1cec9be3be49afb31bde16074e6c5616
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-06-01 17:45:12 +0200

    remove duplicates and unless chars
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- components/AccountPage.vue
+++ components/AccountPage.vue
@@ -8,7 +8,7 @@
             </div>
 
             <div class="w-80 md:w-96 lg:w-[500px]">
-                <div class="">
+                <div>
                 <div class="mb-3">
                     <p class="text-sm text-gray-300 font-semibold mb-1">Neue Universität</p>
                     <p class="border-b-2 w-1/2"></p>
@@ -20,7 +20,7 @@
                         <path d="M20,18V17H19V10h1V7L12,2.08,4,7v3H5v7H4.2v1H3v3H21V18ZM12,4l3.14,2H8.86Zm6,15H6a.5.5,0,0,1,0-1H18a.5.5,0,0,1,0,1ZM7,17V11H8v6Zm3,0V11h1v6Zm3,0V11h1v6Zm3,0V11h1v6Zm2-8H6.5a.5.5,0,0,1,0-1H18a.5.5,0,0,1,0,1Z"/></svg>
                     </span>
 
-                    <input type="text" class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Deine neue Universität">
+                    <input type="text" class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Deine neue Universität">
                 </div>
                 </div>
 
@@ -35,7 +35,7 @@
                         <img src="/assets/img/book.svg" class="w-6 h-6 mx-3" alt="">
                     </span>
 
-                    <input type="text" class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Dein neuer Studiengang">
+                    <input type="text" class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Dein neuer Studiengang">
                 </div>
                 </div>
             </div>
--- components/Table.vue
+++ components/Table.vue
@@ -8,16 +8,16 @@
             <img src="/assets/img/book.svg" class="w-6 h-6 mx-3" alt="">
           </span>
 
-          <input class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Studienfach" type="text" readonly>
+          <input class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Studienfach" type="text" readonly>
         </div>
         
 
         <div class="flex justify-between items-center gap-4 md:gap-10 lg:gap-16">
-          <input class="block w-full py-3 border rounded-lg bg-transparent text-center text-gray-300 border-gray-600 focus:border-fom focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Credits" type="text" readonly>
+          <input class="block w-full py-3 border rounded-lg bg-transparent text-center text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Credits" type="text" readonly>
           <span class="font-main font-bold text-white text-2xl mt-2">*</span>
-          <input class="block w-full py-3 border rounded-lg bg-transparent text-center text-gray-300 border-gray-600 focus:border-fom focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Note" type="text">
+          <input class="block w-full py-3 border rounded-lg bg-transparent text-center text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Note" type="text">
           <span class="font-main font-bold text-white text-2xl">=</span>
-          <input class="block w-full py-3 border rounded-lg bg-transparent text-center text-gray-300 border-gray-600 focus:border-fom focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Wertung" type="text" readonly>
+          <input class="block w-full py-3 border rounded-lg bg-transparent text-center text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Wertung" type="text" readonly>
         </div>
       </div>
     </div>
