5ab4427564194e558381fa21a664a2d2b8ca8ab1
Reza Jaber <frontendrezajaber@gmail.com>
2023-06-01 17:52:25 +0200

    feat(account): add account page (#11)
    On this page the user will have the opportunity to change some of his
    infos, for example the university and the major, this is because people
    do change them, at latest when they switch to a masters for example.
    
    Closes
    [AB#96](https://dev.azure.com/SmartGrades/06443d1c-9f71-4946-998b-3bfbf3b72e38/_workitems/edit/96),
    [AB#97](https://dev.azure.com/SmartGrades/06443d1c-9f71-4946-998b-3bfbf3b72e38/_workitems/edit/97)
    and
    [AB#98](https://dev.azure.com/SmartGrades/06443d1c-9f71-4946-998b-3bfbf3b72e38/_workitems/edit/98)
    
    ---------
    
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
    Co-authored-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

---
+++ components/TableResult.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="px-6 xl:px-20 pb-16">
+    <p class="border-t w-full"></p>
+    
+    <div class="md:flex md:justify-center md:items-center md:space-x-6 lg:space-x-14">
+      <div class="mt-8 flex justify-between gap-6 lg:gap-14">
+        <input class="block w-full py-3 border rounded-lg bg-transparent text-center text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Credits" type="text" readonly>
+        <input class="block w-full py-3 border rounded-lg bg-transparent text-center text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Note" type="text" readonly>
+      </div>
+
+      <button class="mt-4 py-4 w-full bg-fom rounded-lg text-white font-bold cusor-pointer text-sm md:mt-8 md:w-[179px]">Download Excel</button>
+    </div>
+  </div>
+</template>
---
+++ pages/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="h-full bg-dark">
+      <TableNav />
+      <Table />
+      <Table />
+      <Table />
+      <Table />
+      <Table />
+      <Table />
+      <Table />
+      <TableResult />
+    </div>
+  </div>
+</template>
