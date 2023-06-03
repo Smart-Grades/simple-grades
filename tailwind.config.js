7234a515cd3c89ca75e34faf7ca13e00c24bf7c5
Lapotor <17144397+Lapotor@users.noreply.github.com>
2023-06-03 16:40:28 +0200

    style: add ESLint and EditorConfig and restyle files according to rules (#19)
    This commit introduces the addition of ESLint and EditorConfig to the
    project. The ESLint configuration will enable code quality and
    consistency enforcement, while EditorConfig will ensure consistent
    coding styles across various editors.
    
    All existing project files have been restyled in accordance with the
    newly implemented rules. This includes formatting, indentation, and
    other code style conventions.
    
    These changes have been made to enhance code maintainability and
    readability.
    
    ---------
    
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      'main': ['Montserrat', 'sans-serif'],
-      'fancy': ['Source Serif Pro', 'serif']
+      main: ["Montserrat", "sans-serif"],
+      fancy: ["Source Serif Pro", "serif"],
     },
     fontWeight: {
       light: 300,
@@ -20,31 +20,31 @@ module.exports = {
       semibold: 600,
       bold: 700,
       extrabold: 800,
-      black: 900
+      black: 900,
     },
     fontSize: {
-      '2xs': '.625rem',
-      'xs': '.75rem',
-      'sm': '.875rem',
-      'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
-      '2xl': '1.5rem',
-      '3xl': '1.875rem',
-      '4xl': '2.25rem',
-      '5xl': '3rem',
-      '6xl': '4rem',
-      '7xl': '4.5rem',
-      '8xl': '5.5rem',
-      '9xl': '7rem',
-      'xxxl': '8.5rem'
+      "2xs": ".625rem",
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "4.5rem",
+      "8xl": "5.5rem",
+      "9xl": "7rem",
+      xxxl: "8.5rem",
     },
     extend: {
       colors: {
-        dark: '#1b1b1b',
-        fom: '#00352e'
-      }
+        dark: "#1b1b1b",
+        fom: "#00352e",
+      },
     },
   },
   plugins: [],
-}
+};
