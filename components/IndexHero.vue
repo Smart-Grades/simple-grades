99123ef0e61739807ae205d58cd1f1e9d75a289d
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-06-20 17:53:37 +0200

    fix format to default
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- components/IndexHero.vue
+++ components/IndexHero.vue
@@ -5,7 +5,7 @@
       <h1
         class="mb-1 font-fancy font-bold text-white text-4xl leading-10 sm:mb-4 sm:text-6xl sm:leading-[4.5rem] md:mb-4 md:text-7xl md:leading-[5rem] lg:mb-8 lg:text-9xl lg:leading-[7rem]"
       >
-       Der perfekte Überblick <br />für die Uni.
+        Der perfekte Überblick <br />für die Uni.
       </h1>
       <h5
         class="font-main text-gray-200 text-base leading-10 sm:text-xl md:text-2xl"
@@ -15,13 +15,13 @@
     </div>
     <!-- GetStarted-Button -->
     <div class="flex justify-center">
-        <NuxtLink to="/auth">
-            <button
-            class="font-display font-semibold text-sm text-white shadow-2xl bg-fom py-2 px-6 rounded-md transition ease-out duration-300 hover:scale-105 sm:py-3 sm:px-7 sm:text-base lg:text-lg"
-            >
-              Loslegen
-            </button>
-        </NuxtLink>
+      <NuxtLink to="/auth">
+        <button
+          class="font-display font-semibold text-sm text-white shadow-2xl bg-fom py-2 px-6 rounded-md transition ease-out duration-300 hover:scale-105 sm:py-3 sm:px-7 sm:text-base lg:text-lg"
+        >
+          Loslegen
+        </button>
+      </NuxtLink>
     </div>
     <div class="flex justify-center">
       <hr class="mt-10 w-5/6 lg:mt-14 2xl:mt-16" />
