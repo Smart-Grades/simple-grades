e0daa7ab576056bb94460296d1e1eade4e4970c3
Reza Jaber <frontendrezajaber@gmail.com>
2023-06-20 17:36:30 +0200

    feat: create Password Reset Page (#31)
    This commit implements a new feature for our application by creating the Password Reset Page. Users who are undergoing the password recovery process will now be directed to this page, where they can securely reset their password.
    
    To complete the process, users are required to enter their new password and confirm it once more.
    ---------
    
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
    Co-authored-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

---
+++ components/ResetPassword.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <!-- MAIN -->
+    <div class="">
+      <div class="mb-4 flex-col items-center justify-center">
+        <p class="font-main text-xl text-white font-bold cursor-pointer">
+          Neues Passwort erstellen?
+        </p>
+      </div>
+
+      <div class="w-80 md:w-96 lg:w-[500px]">
+        <div>
+          <div class="mb-3">
+            <p class="text-sm text-gray-300 font-semibold mb-1">
+              Neues Passwort
+            </p>
+            <p class="border-b-2 w-1/2"></p>
+          </div>
+
+          <div class="relative flex items-center">
+            <span class="absolute px-3.5"> 🗝️ </span>
+
+            <input
+              type="password"
+              class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="Neues Passwort"
+            />
+          </div>
+        </div>
+
+        <div class="mt-4">
+          <div class="mb-3">
+            <p class="text-sm text-gray-300 font-semibold mb-1">
+              Passwort wiederholen
+            </p>
+            <p class="border-b-2 w-1/2"></p>
+          </div>
+
+          <div class="relative flex items-center">
+            <span class="absolute px-3.5"> 🗝️ </span>
+
+            <input
+              type="password"
+              class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="Passwort wiederholen"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-8">
+        <button
+          class="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-fom rounded-lg hover:opacity-80 hover:transition hover:ease-out-in hover:duration-250 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+        >
+          Get Started
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
