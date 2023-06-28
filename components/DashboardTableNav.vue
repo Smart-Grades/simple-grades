3e669346a787c7adb77a3a041531f490860eb273
Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
2023-06-29 00:47:04 +0200

    fixed data change button link
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- components/DashboardTableNav.vue
+++ components/DashboardTableNav.vue
@@ -7,7 +7,7 @@
       </p>
 
       <div class="flex space-x-4 items-center">
-        <NuxtLink to="/ident">
+        <NuxtLink to="/account">
           <button
             class="font-main font-bold text-2xs shadow-2xl bg-fom border border-fom text-white w-16 py-1 rounded-md transition ease-out duration-300 hover:scale-105 sm:w-20 sm:py-2 sm:text-xs lg:w-28 lg:py-2.5 lg:text-xs"
           >
