b3ed5fac0b0a18efc2cefed7eac4f0796839a7bb
Lapotor <17144397+Lapotor@users.noreply.github.com>
2023-06-28 10:01:45 +0200

    feat: Add password recovery functionality (#48)
    Implemented the ability to recover passwords in the SmartsGrades app.
    Users can now initiate the password recovery process and reset their
    passwords if they have forgotten them. This feature enhances the user
    experience and improves security.
    
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- components/RestoreEmail.vue
+++ components/RestoreEmail.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- MAIN -->
-    <div class="">
+    <form class="" @submit.prevent="handleInputChange">
       <div class="mb-6 flex-col items-center justify-center">
         <p class="font-main text-xl text-white font-bold cursor-pointer">
           Passwort vergessen?
@@ -22,6 +22,7 @@
             <span class="absolute scale-150 px-3 mb-2"> 📨 </span>
 
             <input
+              v-model="INPUT.email"
               type="email"
               class="block w-full py-3 border rounded-lg px-11 bg-transparent text-gray-300 border-gray-600 focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
               placeholder="max.mustermann@gmail.com"
@@ -34,9 +35,58 @@
         <button
           class="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-fom rounded-lg hover:opacity-80 hover:transition hover:ease-out-in hover:duration-250 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
         >
-          Get Started
+          Reset password
         </button>
       </div>
-    </div>
+    </form>
   </div>
 </template>
+
+<script setup>
+import { Account, Client } from "appwrite";
+
+const APP_CLIENT = new Client();
+const APP_ACCOUNT = new Account(APP_CLIENT);
+const RUNTIME_CONFIG = useRuntimeConfig();
+const SNACKBAR = useSnackbar();
+
+APP_CLIENT.setEndpoint(RUNTIME_CONFIG.public.appwriteEndpoint).setProject(
+  RUNTIME_CONFIG.public.appwriteProject
+);
+
+const INPUT = reactive({
+  email: "",
+});
+
+const getVerifyUrl = () => {
+  return `${window.location.protocol}//${window.location.host}/reset`;
+};
+
+const sendResetEmail = async (email) => {
+  try {
+    await APP_ACCOUNT.createRecovery(email, getVerifyUrl());
+    SNACKBAR.add({
+      text: "Passwort zurücksetzen Email wurde versendet!",
+      type: "success",
+    });
+    await navigateTo("/");
+  } catch (error) {
+    console.error(error);
+    SNACKBAR.add({
+      text: error.message,
+      type: "error",
+    });
+  }
+};
+
+const handleInputChange = async () => {
+  if (INPUT.email === "") {
+    SNACKBAR.show({
+      text: "Bitte Email angeben!",
+      type: "error",
+    });
+    return;
+  }
+  await sendResetEmail(INPUT.email);
+};
+</script>
