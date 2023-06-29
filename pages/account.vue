9262f1d9385f451afb90425c98a569d5c17f8a00
rezajaber <frontendrezajaber@gmail.com>
2023-06-29 02:35:05 +0200

    Added back to dashboard button

--- pages/account.vue
+++ pages/account.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="h-full bg-dark">
+    <NuxtLink to="/dashboard">
+      <p
+        class="text-gray-200 font-main font-bold text-md px-6 pt-6 hover:opacity-70 duration-300"
+      >
+        &lt; Zurück
+      </p>
+    </NuxtLink>
     <div class="py-12 flex h-full xl:h-screen justify-center items-center">
       <div
         class="flex flex-col xl:flex-row justify-center items-start gap-16 xl:32"
@@ -15,7 +22,7 @@
 const isLoggedIn = useState("isLoggedIn");
 
 if (!isLoggedIn.value) {
-  // await navigateTo("/auth");
+  await navigateTo("/auth");
 }
 </script>
 
