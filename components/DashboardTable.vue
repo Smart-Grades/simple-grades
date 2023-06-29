d8fafeeb214917ff0b1df44e722400f69457e5df
Reza Jaber <frontendrezajaber@gmail.com>
2023-06-29 02:27:15 +0200

    feat: Add delete button, add title to inputs, update button style, improve site responsiveness, and replace icons with emojis (#64)
    This commit brings multiple enhancements to the SmartsGrades app.
    Firstly, a delete button has been added, allowing users to easily remove
    specific elements or data entries. Secondly, titles have been added to
    inputs, providing improved clarity and guidance for users. Additionally,
    the style of the button has been updated, enhancing its visual appeal
    and aligning it with the overall app design. Furthermore, efforts have
    been made to improve site responsiveness, ensuring optimal user
    experience across various devices and screen sizes. Lastly, icons have
    been replaced with emojis, adding a touch of creativity and enhancing
    the app's visual aesthetics.
    
    By adding a delete button, providing input titles, updating the button
    style, improving site responsiveness, and incorporating emojis, this
    commit enhances the overall functionality, user experience, and visual
    appeal of the SmartsGrades app.
    
    BREAKING CHANGE: add package `vue-float-label`
    
    Closes #56, #57, #58
    
    ---------
    
    Signed-off-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>
    Co-authored-by: Valentin Sickert <17144397+Lapotor@users.noreply.github.com>

--- components/DashboardTable.vue
+++ components/DashboardTable.vue
@@ -1,62 +1,100 @@
 <template>
   <div class="px-8 mb-8">
     <!-- TABLE -->
-    <div class="md:mt-16">
+    <div class="mt-16 md:mt-16 flex items-center space-x-8 justify-center">
       <div
-        class="space-y-3 md:space-y-0 md:flex md:justify-center md:gap-10 lg:gap-16"
+        class="space-y-10 lg:space-y-0 lg:flex md:justify-center md:gap-10 lg:gap-16"
       >
         <div class="flex items-center">
-          <span class="absolute">
-            <img src="/assets/img/book.svg" class="w-6 h-6 mx-3" alt="" />
-          </span>
+          <span class="absolute scale-150 px-4"> 📖 </span>
 
-          <input
-            :value="course"
-            class="block w-full py-3 border rounded-lg px-11 bg-transparent text-white border-white focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
-            placeholder="Studienfach"
-            type="text"
-            @blur="$emit('update:course', $event.target.value)"
-            @input="shouldReset = false"
-          />
+          <FloatLabel class="w-full">
+            <input
+              :value="course"
+              class="block w-full lg:w-[28rem] xl:w-[38rem] py-3 border rounded-lg px-11 bg-transparent text-white border-white focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="Studienfach"
+              type="text"
+              @blur="$emit('update:course', $event.target.value)"
+              @input="shouldReset = false"
+            />
+          </FloatLabel>
         </div>
 
-        <div
-          class="flex justify-between items-center gap-4 md:gap-10 lg:gap-16"
-        >
-          <input
-            :value="credits"
-            class="block w-full py-3 border rounded-lg bg-transparent text-center text-white border-white focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
-            placeholder="Credits"
-            type="text"
-            :readonly="id === 'new'"
-            @blur="$emit('update:credits', $event.target.value)"
-          />
-          <span class="font-main font-bold text-white text-2xl mt-2">*</span>
-          <input
-            :value="grade"
-            class="block w-full py-3 border rounded-lg bg-transparent text-center text-white font-bold border-white focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
-            placeholder="Note"
-            type="number"
-            step="0.1"
-            :readonly="id === 'new'"
-            @blur="$emit('update:grade', $event.target.value)"
-          />
-          <span class="font-main font-bold text-white text-2xl">=</span>
-          <input
-            :value="((credits / totalCredits) * 100).toFixed(2)"
-            class="block w-full py-3 border rounded-lg bg-transparent text-center text-white border-white focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
-            placeholder="Wertung"
-            type="text"
-            readonly
-          />
+        <div class="lg:flex lg:items-center lg:space-x-8">
+          <div
+            class="flex justify-between items-center gap-4 md:gap-10 lg:gap-16"
+          >
+            <FloatLabel>
+              <input
+                :value="credits"
+                class="block w-full py-3 border rounded-lg bg-transparent text-center text-white border-white focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Credits"
+                type="text"
+                :readonly="id === 'new'"
+                @blur="$emit('update:credits', $event.target.value)"
+              />
+            </FloatLabel>
+
+            <FloatLabel>
+              <input
+                :value="grade"
+                class="block w-full py-3 border rounded-lg bg-transparent text-center text-white font-bold border-white focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Note"
+                type="number"
+                step="0.1"
+                :readonly="id === 'new'"
+                @blur="$emit('update:grade', $event.target.value)"
+              />
+            </FloatLabel>
+
+            <span class="font-main font-bold text-white text-2xl">|</span>
+
+            <FloatLabel>
+              <input
+                :value="((credits / totalCredits) * 100).toFixed(2)"
+                class="block w-full py-3 border rounded-lg bg-transparent text-center text-white border-white focus:border-fom focus:ring-fom focus:outline-none focus:ring focus:ring-opacity-40"
+                placeholder="Wertung"
+                type="text"
+                readonly
+              />
+            </FloatLabel>
+          </div>
         </div>
       </div>
+
+      <svg
+        v-if="id !== 'new'"
+        version="1.0"
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-8 h-8 fill-gray-200 hover:fill-red-800 cursor-pointer hover:scale-125 active:scale-110 duration-300 ease-out-in"
+        viewBox="0 0 1280.000000 1280.000000"
+        preserveAspectRatio="xMidYMid meet"
+        @click="$emit('delete')"
+      >
+        <g
+          transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)"
+          stroke="none"
+        >
+          <path
+            d="M3137 9659 l-537 -539 1360 -1360 1360 -1360 -1360 -1360 -1360
+-1360 540 -540 540 -540 1360 1360 1360 1360 1360 -1360 1360 -1360 540 540
+540 540 -1360 1360 -1360 1360 1360 1360 1360 1360 -540 540 -540 540 -1360
+-1360 -1360 -1360 -1360 1360 c-748 748 -1361 1360 -1362 1359 -2 0 -245 -243
+-541 -540z"
+          />
+        </g>
+      </svg>
+      <div v-if="id === 'new'" class="w-8"></div>
     </div>
   </div>
 </template>
 
 <script>
+import FloatLabel from "vue-float-label/components/FloatLabel";
 export default {
+  components: {
+    FloatLabel,
+  },
   props: {
     id: {
       type: String,
@@ -97,3 +135,15 @@ export default {
   },
 };
 </script>
+
+<style>
+.vfl-label {
+  text-transform: uppercase;
+}
+.vfl-label-on-input {
+  top: -1.5rem;
+}
+.vfl-label-on-focus {
+  color: #ffffff;
+}
+</style>
--- components/DashboardTableNav.vue
+++ components/DashboardTableNav.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="px-6 pt-6 mb-8">
+  <div class="px-5 md:px-12 pt-8 mb-8">
     <!-- HEADER -->
-    <div class="flex justify-between">
-      <p class="font-main text-xl text-white font-bold cursor-pointer">
-        SmartGrades
-      </p>
+    <div class="flex justify-between items-center">
+      <NuxtLink to="/">
+        <p class="font-main text-xl text-white font-bold cursor-pointer">
+          SmartGrades
+        </p>
+      </NuxtLink>
 
-      <div class="flex space-x-4 items-center">
+      <div class="flex space-x-2 items-center">
         <NuxtLink to="/account">
           <button
-            class="font-main font-bold text-2xs shadow-2xl bg-fom border border-fom text-white w-16 py-1 rounded-md transition ease-out duration-300 hover:scale-105 sm:w-20 sm:py-2 sm:text-xs lg:w-28 lg:py-2.5 lg:text-xs"
+            class="font-main font-bold text-2xs shadow-2xl bg-fom border border-fom text-white w-20 sm:w-24 py-1 rounded-md transition ease-out duration-300 hover:scale-105 sm:py-2 md:w-24 sm:text-xs lg:w-28 lg:py-2.5 lg:text-xs"
           >
             Daten ändern
           </button>
